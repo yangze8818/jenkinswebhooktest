b86186f449ccc7fea2b00c65fdf9ce89f56d95c7
yangze8818 <yangze_lmkj@163.com>
2018-04-28 13:58:59 +0800

    jenkins webhook test

--- src/main/java/com/yz/JenkinsWebhookStu.java
+++ src/main/java/com/yz/JenkinsWebhookStu.java
@@ -2,6 +2,6 @@ package com.yz;
 
 public class JenkinsWebhookStu {
     public static void main(String[] args) {
-        System.out.println("Jenkins Webhook 2 Study");
+        System.out.println("Jenkins Webhook 3 Study");
     }
 }
